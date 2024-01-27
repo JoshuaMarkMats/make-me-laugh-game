beaac6ac253cdf73306027c12e8c749b5120bb13
Kyle Gordo <kylgordz@gmail.com>
2024-01-27 12:10:40 +0800

    Main Menu UI and Endings
    WE added win lose scene.. have main menu work and how to play plus background

---
+++ Assets/Scripts/ButtonLogics.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLogics : MonoBehaviour
+{
+    public GameObject howToPlay;
+    bool on = false;
+
+    public GameObject page1;
+    public GameObject page2;
+
+    public void StartGame()
+    {
+        Debug.Log("Game Started!!");
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    public void HowToPlay()
+    {
+        on = !on;
+        howToPlay.SetActive(on);
+        Debug.Log("Opening How To Play Instructions!!");
+
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Game Closed!");
+        Application.Quit();
+
+    }
+    public void ReturnMain()
+    {
+        Debug.Log("Back To Main");
+        SceneManager.LoadScene("MainMenu");
+
+
+    }
+
+    public void CloseHowToPlay()
+    {
+        howToPlay.SetActive(false);
+    }
+
+    public void NextPage()
+    {
+        if (page1.activeSelf == true)
+        {
+            page1.SetActive(false);
+            page2.SetActive(true);
+        }
+
+        else if(page1.activeSelf == false)
+        {
+            page1.SetActive(true);
+            page2.SetActive(false);
+        }
+
+    }
+}
