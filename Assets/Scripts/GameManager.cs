0a5826270770944fc9db23cbb33bd9bf19229aad
JoshuaMarkMats <148032670+JoshuaMarkMats@users.noreply.github.com>
2024-01-27 10:08:31 +0800

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,46 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     // Singleton instance of the game manager
     public static GameManager Instance { get; private set; }
 
-    private const string MAIN_MENU_SCENE = "StrangeFarm";
-
-    public enum GameState
-    {
-        MAIN_MENU,
-        PLAYING,
-        PAUSED,
-        GAME_WIN,
-        GAME_OVER,
-        TRANSITION
-    }
-
-    public GameState gameState = GameState.MAIN_MENU;
-
-    [SerializeField]
-    private GameObject gameOverMenu;
-    [SerializeField]
-    private GameObject pausemenu;
-    //public SpeechBox speechBox;
-
-    public UnityEvent gameOverEvent = new();
-    private bool gameFrozen;
-
-    public bool GameFrozen { 
-        get { return gameFrozen; } 
-        set
-        {
-            if (value == true)
-                FreezeGame();
-            else
-                UnfreezeGame();
-        }
-    }
+    public UnityEvent game_overEvent = new();
+    public UnityEvent game_winEvent = new();
 
     private void Awake()
     {
@@ -59,86 +27,9 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 120;
-
-        gameOverEvent.AddListener(GameOver);
+        //game_overEvent.AddListener(CodeBlockManager.Instance.EraseData);
+        //game_winEvent.AddListener(CodeBlockManager.Instance.EraseData);
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown("escape"))
-            OnPause();
-    }
 
-    void GameOver()
-    {
-        gameState = GameState.GAME_OVER;
-        gameOverMenu.SetActive(true);
-        FreezeGame();        
-    }
-
-    public void RestartLevel()
-    {
-        UnfreezeGame();
-        //AudioManager.Instance.StopAll();
-        gameOverMenu.SetActive(false);
-        pausemenu.SetActive(false);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-
-    public void ReturnToMenu()
-    {
-        UnfreezeGame();
-        //AudioManager.Instance.StopAll();
-        gameOverMenu.SetActive(false);
-        pausemenu.SetActive(false);
-        gameState = GameState.MAIN_MENU;
-        SceneManager.LoadScene(MAIN_MENU_SCENE);
-    }
-
-    /*public void GameWin()
-    {
-        gameState = GameState.GAME_WIN;
-        UIManager.Instance.hideTimer();
-        FreezeGame();
-        UIManager.Instance.GameWin();
-    }*/
-
-    //toggle pause
-    public void OnPause()
-    {
-        if (gameState != GameState.PLAYING && gameState != GameState.PAUSED)
-            return;
-
-        if (!gameFrozen)
-        {
-            pausemenu.SetActive(true);
-            FreezeGame();
-            gameState = GameState.PAUSED;
-        }
-        else
-        {
-            pausemenu.SetActive(false);
-            UnfreezeGame();
-            gameState = GameState.PLAYING;
-        }
-    }
-
-    public void FreezeGame()
-    {
-        gameFrozen = true;
-        Time.timeScale = 0f;
-    }
-
-    public void UnfreezeGame()
-    {
-        gameFrozen = false;
-        Time.timeScale = 1f;
-    }
-
-    public void SetFps(int fps)
-    { 
-        Application.targetFrameRate = fps;
-    }
 }
